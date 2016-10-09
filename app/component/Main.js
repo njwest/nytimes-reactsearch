83e86f2f6516567b77b9f437cb7dac0fe01a7d5a
Nick West <mail@nickwe.st>
2016-10-08 23:10:07 -0400

    almost finished

---
+++ app/component/Main.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const Child = require('./Saved.js');
+const axios = require('axios');
+
+let Main = React.createClass({
+  getInitialState: function(){
+    return{
+
+    }
+  },
+
+  handleSearch: function(){
+    this.setState
+  }
+})
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const Article = require('./models/article.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+mongoose.connect('mongodb:nytreact');
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
+
+app.get('/api/saved', function(req, res) {
+  Article.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
+
+app.post('/api/saved', function(req, res) {
+
+});
+
+app.delete('/api/saved', function(req, res){
+
+})
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
